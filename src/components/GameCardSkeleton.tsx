2004b3486589ddac49e61ff937d6d2600ed17d4e
Joseph Rivera <jojo@Josephs-Mac-mini.local>
2024-09-09 14:00:09 -0700

    Refactor: remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,12 +3,11 @@ import React from "react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
-      <Skeleton height="300px">
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Skeleton>
+    <Card>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
     </Card>
   );
 };
